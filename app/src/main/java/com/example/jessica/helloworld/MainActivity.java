535f04789e0ac7182997389de769401337ad068e
Jessica Tekenos <jessicatekenos@gmail.com>
2015-01-07 16:40:01 -0800

    second change

--- app/src/main/java/com/example/jessica/helloworld/MainActivity.java
+++ app/src/main/java/com/example/jessica/helloworld/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -38,9 +40,9 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
 
     }
-
+/*
     public void foo (final View view) {
         Toast.makeTest(getApplication(), "Hello, world!", Toast.LENGTH_SHORT).show();
-    }
+    } */
 }
 
